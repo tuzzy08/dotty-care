d7d312a1b40780850d49151eb7c59cd4c9885e9b
tuzzy08 <tuzzy08@gmail.com>
2023-02-18 13:24:49 +0100

    fixed login page

--- src/pages/dashboard/components/AuthForm.tsx
+++ src/pages/dashboard/components/AuthForm.tsx
@@ -4,6 +4,7 @@ import {
 	TextInput,
 	PasswordInput,
 	Checkbox,
+	Container,
 	Button,
 	Title,
 	Text,
@@ -23,21 +24,10 @@ type Inputs = {
 };
 
 const useStyles = createStyles((theme) => ({
-	wrapper: {
-		minHeight: '100vh',
-		backgroundSize: 'cover',
-		backgroundImage:
-			'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
-	},
-
 	form: {
-		borderRight: `1px solid ${
-			theme.colorScheme === 'dark' ? theme.colors.dark[7] : theme.colors.gray[3]
-		}`,
-		// minHeight: '100vh',
-		height: 200,
-		maxWidth: 450,
-		paddingTop: 80,
+		height: 450,
+		width: 500,
+		paddingTop: 20,
 
 		[`@media (max-width: ${theme.breakpoints.sm}px)`]: {
 			maxWidth: '100%',
@@ -82,45 +72,37 @@ export default function Auth() {
 		}
 	};
 	return (
-		<div className={classes.wrapper}>
-			<Paper className={classes.form} radius={0} p={30}>
-				<Title
-					order={2}
-					className={classes.title}
-					align='center'
+		<Paper className={classes.form} radius={15} p={30}>
+			<Title order={2} className={classes.title} align='center' mt='md' mb={50}>
+				Welcome to Fast Health
+			</Title>
+			{/* Login Form */}
+			<form onSubmit={handleSubmit(handleLogin)}>
+				<TextInput
+					label='Email address'
+					placeholder='hello@gmail.com'
+					size='md'
+					{...register('email')}
+				/>
+				<PasswordInput
+					label='Password'
+					placeholder='Your password'
 					mt='md'
-					mb={50}
-				>
-					Welcome to Fast Health
-				</Title>
-				{/* Login Form */}
-				<form onSubmit={handleSubmit(handleLogin)}>
-					<TextInput
-						label='Email address'
-						placeholder='hello@gmail.com'
-						size='md'
-						{...register('email')}
-					/>
-					<PasswordInput
-						label='Password'
-						placeholder='Your password'
-						mt='md'
-						size='md'
-						{...register('password')}
-					/>
-					{/* <Checkbox label="Keep me logged in" mt="xl" size="md" /> */}
+					size='md'
+					{...register('password')}
+				/>
+				{/* <Checkbox label="Keep me logged in" mt="xl" size="md" /> */}
 
-					<Button fullWidth mt='xl' size='md' type='submit'>
-						Login
-					</Button>
-				</form>
-				<Text align='center' mt='md'>
-					Don&apos;t have an account?{' '}
-					<Link href='/auth/signup'>
-						<Text weight={700}>Register</Text>
-					</Link>
-				</Text>
-			</Paper>
-		</div>
+				<Button fullWidth mt='xl' size='md' type='submit'>
+					Login
+				</Button>
+			</form>
+			<Text align='center' mt='md'>
+				Don&apos;t have an account?{' '}
+				<Link href='/auth/signup'>
+					<Text weight={700}>Register</Text>
+				</Link>
+			</Text>
+		</Paper>
 	);
 }
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,17 @@ import AuthForm from './dashboard/components/AuthForm';
 
 export default function Index() {
 	return (
-		<Box>
+		<Box
+			sx={(theme) => ({
+				display: 'flex',
+				alignItems: 'center',
+				justifyContent: 'center',
+				minHeight: '100vh',
+				backgroundSize: 'cover',
+				backgroundImage:
+					'url(https://images.unsplash.com/photo-1484242857719-4b9144542727?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=1280&q=80)',
+			})}
+		>
 			<AuthForm />
 		</Box>
 	);
